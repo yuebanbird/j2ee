ab8e5ee6f72a3de50e1e44058b175bca4f18ff03
98294 <982942863@qq.com>
2020-03-22 11:14:30 +0800

    test

--- src/HelloServlet.java
+++ src/HelloServlet.java
@@ -12,7 +12,7 @@ public class HelloServlet extends javax.servlet.http.HttpServlet {
 
     protected void doGet(HttpServletRequest request,HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         try {
-            response.getWriter().println("<h1>Hello Servlet!</h1>");
+            response.getWriter().println("<h1>Hello Servlet!???</h1>");
             response.getWriter().println(new Date().toLocaleString());
         } catch (IOException e) {
             // TODO Auto-generated catch block
