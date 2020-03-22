c4b57dc69347694b6d31baf7254a2f6a06c4e034
98294 <982942863@qq.com>
2020-03-22 10:59:46 +0800

    test

---
+++ src/HelloServlet.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+ //   protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+ //   }
+
+    protected void doGet(HttpServletRequest request,HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        try {
+            response.getWriter().println("<h1>Hello Servlet!</h1>");
+            response.getWriter().println(new Date().toLocaleString());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
